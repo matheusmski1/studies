b1f4322e20efb1d7ebe8011ca04d64a985f0298c
Matheus dos Santos <70772084+matheusmski1@users.noreply.github.com>
2025-01-12 20:28:16 -0300

    redis course

---
+++ rbay/src/services/queries/page-cache.ts
@@ -0,0 +1,24 @@
+import { pageCacheKey } from "$services/keys";
+import { client } from "$services/redis";
+
+const cacheRoutes = [
+    '/about', '/privacy', '/auth/signin', '/auth/signup'
+]
+
+export const getCachedPage = (route: string) => {
+    if(cacheRoutes.includes(route)){
+        client.get(pageCacheKey(route))
+    }
+    
+    return null
+};
+
+export const setCachedPage = (route: string, page: string) => {
+    if(cacheRoutes.includes(route)){
+        client.set(pageCacheKey(route), page,{
+            EX: 2
+        })
+    }
+
+    return null
+};
