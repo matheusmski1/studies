c96096191a0ad05e011f241efb90e1a18f60d0ab
Matheus dos Santos <70772084+matheusmski1@users.noreply.github.com>
2025-01-21 11:46:58 -0300

    feat: likes, sessions, users, serializers

--- rbay/src/services/keys.ts
+++ rbay/src/services/keys.ts
@@ -1,1 +1,6 @@
 export const pageCacheKey = (id: string) => `pagecache#${id}`;
+export const usersKey = (userid: string) => `users#${userid}`;
+export const sessionsKey = (sessionsId: string) => `sessions#${sessionsId}`;
+export const itemsKey = (itemsId: string) => `items#${itemsId}`;
+export const usernamesUniqueKey = () => `usernames:unique`;
+export const userLikesKey = (userId: string) => `users:likes#${userId}`
