9017a6fa71c72f8e09a0d75b213040202b3ea798
Matheus dos Santos <70772084+matheusmski1@users.noreply.github.com>
2025-01-30 21:47:32 -0300

    feat: complete

--- rbay/src/services/queries/views.ts
+++ rbay/src/services/queries/views.ts
@@ -1,9 +1,5 @@
 import { client } from '$services/redis';
-import { itemsKey, itemsByViewsKey } from '$services/keys';
 
 export const incrementView = async (itemId: string, userId: string) => {
-	return Promise.all([
-		client.hIncrBy(itemsKey(itemId), 'views', 1),
-		client.zIncrBy(itemsByViewsKey(), 1, itemId)
-	]);
+	return client.incrementView(itemId, userId);
 };
